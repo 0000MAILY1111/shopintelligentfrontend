0c5c748fe64aba5ac359637a916a728d72660602
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-07-08 01:08:15 -0400

    1image

--- app/(store)/[categoryId]/page.tsx
+++ app/(store)/[categoryId]/page.tsx
@@ -1,33 +1,28 @@
-import { CategoryWithProductsResponseSchema } from "@/src/schemas";
+import { CategoryWithProductsResponseSchema } from "@/app/src/schemas";
 
 import ProductCard from "@/components/products/ProductCard";
-
 type Params = Promise<{categoryId: string}>
-
 
 
 async function getProducts (categoryId: string) {
  const url = `${process.env.API_URL}/categories/${categoryId}?products=true`;
-  console.log (url);
-  console.log("categoryId:", categoryId);
+  
   const req = await fetch(url);
-  const jsone = await req.json();
-  const products = CategoryWithProductsResponseSchema.parse(jsone)
+  const json = await req.json();
+  const products = CategoryWithProductsResponseSchema.parse(json)
   return products  ;
 
 }
 
 export default async function StorePage({params}:{params:Params}) {
   const {categoryId} = await params;
-  const category = await getProducts(categoryId);
-
+  const products = await getProducts(categoryId);
 
   return (
     <div className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
-      {category.products.map (product => (
-        <ProductCard
-        key = {product.id}
-        product = {product}/>
+     
+      {products.products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     
     
--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,24 @@ const nextConfig: NextConfig = {
     {
       protocol: 'http',
       hostname: process.env.DOMAIN!,
-  
+    },
+    {
+      protocol: 'https',
+      hostname: process.env.DOMAIN!,
+    },
+    {
+      protocol: 'http',
+      hostname: 'localhost',
+      port: '3000',
+    },
+    {
+      protocol: 'https',
+      hostname: 'localhost',
+      port: '3000',
     }
+  
+  
+
    ]
       
 
