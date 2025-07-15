d94d9dbd24167943a774b0c79d8903a8e94182ce
0000MAILY1111 <valdezmelizza19@gmail.com>
2025-07-15 15:58:21 -0400

    createpaginationproducts

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,27 +1,41 @@
 import ProductsTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
+import Pagination from "@/components/ui/Pagination";
 import { ProductResponseSchema } from "@/src/schemas";
+import { isValidPage } from "@/src/utils";
+import { redirect } from "next/navigation";  ///import next-navigation
 
-async function getProducts() {
-    const url =  `${process.env.API_URL}/products`
-    const req = await fetch (url)
+async function getProducts(take: number, skip: number) {
+    const url = `${process.env.API_URL}/products?take=${take}$skip=${skip}`
+    const req = await fetch(url)
     const json = await req.json()
     const data = ProductResponseSchema.parse(json)
     return {
-        products : data.products,
-        total : data.total
+        products: data.products,
+        total: data.total
     }
 }
-export default async function ProductsPage(){
-    const {products, total} = await getProducts()
+
+type SearchParams = Promise<{ page: string }>
+
+export default async function ProductsPage({searchParams}: {searchParams: SearchParams}) {
+    const { page } = await searchParams
+    if (!isValidPage(+page)) redirect('/admin/product?page=1')
+    const productsPerPage = 10
+    const skip = (+page - 1) * productsPerPage
+    const { products, total } = await getProducts(productsPerPage, skip)
+
+    const totalPages = Math.ceil(total / productsPerPage)
+    if (+page > totalPages) redirect('/admin/products?page=1')
     return (
         <>
-        <Heading>
-            Administrar Productos 
-        </Heading>
-
-        <ProductsTable
-           products = {products}/>
+            <Heading>Administrar Productos</Heading>
+            <ProductsTable
+                products={products} />
+            <Pagination
+                page={+page}   ///+ para que lo tome como string cualquier numero que se pase 
+                totalPages={totalPages}
+            />
         </>
     )
 }
